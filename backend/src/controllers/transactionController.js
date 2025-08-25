1d217dd8d62847b9ef2445ec758df90b11ff144b
jeronfalcao <falcaojeron2912@gmail.com>
2025-08-25 12:06:58 +0530

    edited more

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -22,33 +22,7 @@ export const getTransaction = async(req, res) => {
     }
 }
 
-export const deleteTransaction = async(req, res) => {
-    try {
-        const { id } = req.params
 
-        if (!Number.isInteger(Number(id))) {
-            return res.status(400).json({
-                message: "Invalid transactions ID"
-            })
-        }
-
-        const result = await sql `
-      DELETE FROM transactions WHERE id =${id} RETURNING *
-    `
-
-        if (result.length === 0) {
-            return res.status(404).json({
-                message: "transaction not found"
-            })
-        }
-        res.status(200).json({
-            message: "Transaction deleted successfully"
-        })
-    } catch (error) {
-        console.error("Error fetching transaction:", error);
-        res.status(500).json({ message: "error", error: error.message });
-    }
-}
 
 export const transactionSummary = async(req, res) => {
     try {
