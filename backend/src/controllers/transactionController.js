f1a269e3415a78611aa0fb9952d0e90343f9a9e4
jeronfalcao <falcaojeron2912@gmail.com>
2025-08-25 12:08:51 +0530

    made changes in the delete route and updated it

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -1,6 +1,27 @@
 import { sql } from "../config/db.js";
 
 
+export const addTransaction = async(req, res) => {
+    try {
+        const { title, amount, category, user_id } = req.body;
+
+        if (!title || !user_id || !category || amount === undefined) {
+            return res.status(400).json({ message: "All feilds are required" });
+        }
+
+        const transaction = await sql `
+      INSERT INTO transactions(user_id,title,amount,category)
+      VALUES(${user_id}, ${title}, ${amount}, ${category})
+      RETURNING *
+    `;
+
+        res.status(201).json(transaction[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "error" });
+
+    }
+}
 
 
 
@@ -22,7 +43,33 @@ export const getTransaction = async(req, res) => {
     }
 }
 
+export const deleteTransaction = async(req, res) => {
+    try {
+        const { id } = req.params
 
+        if (!Number.isInteger(Number(id))) {
+            return res.status(400).json({
+                message: "Invalid transactions ID"
+            })
+        }
+
+        const result = await sql `
+      DELETE FROM transactions WHERE id =${id} RETURNING *
+    `
+
+        if (result.length === 0) {
+            return res.status(404).json({
+                message: "transaction not found"
+            })
+        }
+        res.status(200).json({
+            message: "Transaction deleted successfully"
+        })
+    } catch (error) {
+        console.error("Error fetching transaction:", error);
+        res.status(500).json({ message: "error", error: error.message });
+    }
+}
 
 export const transactionSummary = async(req, res) => {
     try {
