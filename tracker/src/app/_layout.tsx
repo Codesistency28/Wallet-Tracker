5e8f5f4fbb92a1793d5a73aad6e039424ad1373f
Shonferns004 <shawnferns004@gmail.com>
2025-09-16 11:47:13 +0530

    issu no 7 solved

---
+++ tracker/src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Slot, Stack } from "expo-router";
+import "@/global.css"
+import { ClerkProvider} from "@clerk/clerk-expo"
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot/>
+    </ClerkProvider>
+  );
+}
